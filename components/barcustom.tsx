e46db2c3b80727c05d75a86a6b44a67fbf7ea32b
Ramon Barbosa Souza <ramon.barbosa@w5i.com.br>
2025-05-24 23:39:32 -0300

    feat: adicionar animação e interatividade ao componente BarCustom com entrada de texto animada e lógica de legenda dinâmica

--- components/barcustom.tsx
+++ components/barcustom.tsx
@@ -1,7 +1,8 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TextInput } from 'react-native';
 import React from 'react';
-import { CartesianChart, Bar } from 'victory-native';
+import { CartesianChart, Bar, useChartPressState } from 'victory-native';
 import { useFont } from '@shopify/react-native-skia';
+import Animated, { useAnimatedProps } from 'react-native-reanimated';
 
 type Props = {
   data: { tag: string; count: number }[];
@@ -15,8 +16,19 @@ const colors = [
   '#E76F51', // outro tom
 ];
 
+Animated.addWhitelistedNativeProps({text: true})
+const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
+
 const BarCustom = ({ data }: Props) => {
-  const font = useFont(require('@/assets/fonts/SpaceMono-Regular.ttf'), 12);
+    const font = useFont(require('@/assets/fonts/SpaceMono-Regular.ttf'), 12);
+    const { state, isActive} = useChartPressState<{ x: number; y: Record<"y", number> }>({ x: 0, y: { y: 0 } });
+
+     const animatedText =  useAnimatedProps( () => {
+        return{
+        text: `Contagem: ${state.y.y.value.value}`,
+            defaultValue: ""
+        }
+    }) 
 
   const xLabels = data.map(d => d.tag);
   const chartData = data.map((item, index) => ({
@@ -32,12 +44,13 @@ const BarCustom = ({ data }: Props) => {
   return (
     <View style={styles.container}>
       <CartesianChart
-
         data={chartData}
         xKey="x"
         yKeys={['y']}
         domainPadding={{ left: 50, right: 50, top: 30 }}
         domain={{ y: [0, maxCount + 2] }}
+        chartPressState={state}
+
         axisOptions={{
           font,
           formatXLabel: (value) => xLabels[value] ?? '',
@@ -53,21 +66,36 @@ const BarCustom = ({ data }: Props) => {
               topLeft: 5,
               topRight: 5,
             }}
+            
           />
         )}
       </CartesianChart>
 
       {/* Legenda */}
       <View style={styles.legendContainer}>
-        {chartData.map((item, index) => (
-          <View key={index} style={styles.legendItem}>
-            <View
-              style={[styles.colorBox, { backgroundColor: item.color }]}
-            />
-            <Text style={styles.legendText}>{item.x !== undefined ? xLabels[item.x] : ''}</Text>
-          </View>
-        ))}
+              { isActive ?
+                <View>
+                    <AnimatedTextInput
+                        editable={false}
+                        underlineColorAndroid={"transparent"}
+                        style={styles.legendText}
+                        animatedProps={animatedText}
+                    >
+                    </AnimatedTextInput>
+                    
+                </View>
+                :
+                chartData.map((item, index) => (
+                    <View key={index} style={styles.legendItem}>
+                        <View
+                        style={[styles.colorBox, { backgroundColor: item.color }]}
+                        />
+                        <Text style={styles.legendText}>{item.x !== undefined ? xLabels[item.x] : ''}</Text>
+                    </View>
+                    ))
+        }
       </View>
+    
     </View>
   );
 };
@@ -76,7 +104,7 @@ export default BarCustom;
 
 const styles = StyleSheet.create({
   container: {
-    height: 380, // aumentei um pouco pra caber legenda
+    height: 380,
     width: '100%',
     backgroundColor: 'white',
     padding: 10,
@@ -86,7 +114,7 @@ const styles = StyleSheet.create({
   legendContainer: {
     marginTop: 15,
     flexDirection: 'row',
-    flexWrap: 'wrap', // permite quebrar linha se precisar
+    flexWrap: 'wrap', 
     justifyContent: 'center',
   },
   legendItem: {
@@ -105,4 +133,11 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: '#333',
   },
+   textinput:
+    {
+        fontSize:30,
+        fontWeight: "bold",
+        color: "#000",
+    }
+
 });
